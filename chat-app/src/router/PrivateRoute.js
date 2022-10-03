e72a75568b01fb5153ef2a49543f5bd6b14278c7
BrandonGF <jairgrande.22@gmail.com>
2022-10-03 01:10:44 -0500

    Initial dev

---
+++ chat-app/src/router/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    Route,
+    Redirect
+  } from 'react-router-dom';
+  
+  
+export const PrivateRoute = ({
+    isAuthenticated,
+    component:Component,
+    ...rest 
+}) => {
+
+    return (
+ 
+        <Route {...rest}
+            component={(props) =>{
+                return(isAuthenticated) ? <Component {...props}/>
+                : <Redirect to="/auth"/>
+
+}}
+        />
+    )
+}
